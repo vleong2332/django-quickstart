0fc55ffe67a351d637ff93983f5186c8406e707d
VICKY LEONG <VLEONG2332@GMAIL.COM>
2016-11-09 23:32:33 -0500

    Add basic list view

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
 
-# Create your views here.
+from blog.models import Post
+
+
+class PostListView(ListView):
+	model = Post
